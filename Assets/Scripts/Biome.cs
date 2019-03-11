d8fb77618d9b4e2c141e614a46cb94a539a7286a
Carlos Dominguez <dominguerilla@gmail.com>
2019-03-10 21:27:39 -0400

    init

---
+++ Assets/Scripts/Biome.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName ="New Biome", menuName ="Biome")]
+public class Biome : ScriptableObject{
+    
+    public enum LEVEL{
+        LOW,
+        MEDIUM,
+        HIGH
+    }
+
+    public string name;
+    public float averageTemp;
+    public float averagePrecipitationPerYear;
+    public LEVEL averageHumidity;
+    public LEVEL vegetationLevel;
+
+}
